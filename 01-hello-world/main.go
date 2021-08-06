76ae1a8cbdc11738c55ba96c6c3420b8c676e5ed
azemoning <ahmadsaufimaulana@gmail.com>
2021-08-06 10:43:48 +0700

    update 01

--- 01-hello-world/main.go
+++ 01-hello-world/main.go
@@ -3,5 +3,10 @@ package main
 import "fmt"
 
 func main() {
+
+	// function main must be within the same package main
+	// so if we want to create func main()
+	// then we need to place it under the main package
+
 	fmt.Println("Hello, World!")
 }
