4f7b29ebeaef093b6b3b5e4fc353a3b8456c2344
azemoning <ahmadsaufimaulana@gmail.com>
2021-08-06 10:42:45 +0700

    add 02

---
+++ 02-type-number/floating-point/float.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// There is two type on floating point number
+	// float32 & float64
+	// complex32 & complex64
+
+	fmt.Println("floating point number: ", 123.456)
+}
---
+++ 02-type-number/integer/integer.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// There is two types of integer
+	// signed integer (int)
+	// unsigned integer (uint)
+
+	fmt.Println("int: ", -12345)
+	fmt.Println("uint: ", 12345)
+}
