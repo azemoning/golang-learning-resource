a436fbdfba7ac7156e0f181804d398bae42a3bc3
azemoning <ahmadsaufimaulana@gmail.com>
2021-08-06 10:44:33 +0700

    add 03

---
+++ 03-type-boolean/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// Boolean type only have two values
+	// true
+	// false
+
+	fmt.Println("Boolean true: ", true)
+	fmt.Println("Boolean false: ", false)
+}
